50ae88b510fa60d5d8f751e7d761af794dc1bc0b
artpolly1 <art.polly1@gmail.com>
2019-07-12 19:13:04 -0500

    updated files and file names

---
+++ build/precache-manifest.d2cc9ec10679e88d3f70fc571b47a1b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1267fc0dd66ed6c45dd5bb560d6e7fdf",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "df9a71d81cb32b34ff55",
+    "url": "/clickygame/static/css/main.705f0e99.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "df9a71d81cb32b34ff55",
+    "url": "/clickygame/static/js/main.49ffe525.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Card from "./components/Card/Index"
 import Header from "./components/Header"
 import Wrapper from "./components/Wrapper"
 import cards from "./cards.json";
-import './App.css';
+
 
 class App extends Component {
 
